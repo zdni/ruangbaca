a4be0ae5ac61bced63b3d21292de98cae2f7c3d3
Al Zidni Kasim <81102117+zdni@users.noreply.github.com>
2022-11-18 15:15:26 +0800

    delete

--- application/views/templates/web_template.php
+++
@@ -1,6 +0,0 @@
-<?php defined('BASEPATH') or exit('No direct script access allowed');
-$this->load->view('templates/web/head');
-$this->load->view('templates/web/header');
-?>
-<?php echo $view_content; ?>
-<?php $this->load->view('templates/web/footer'); ?>
